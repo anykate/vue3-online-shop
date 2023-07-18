026eb9af8dfdfea6c7c5d2281afb08e0ec66c80d
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-07-18 20:14:39 +0530

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import App from '@/App.vue'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import { createApp } from 'vue'
+
+createApp(App).mount('#app')
